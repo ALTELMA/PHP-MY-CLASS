04f17e8221c6422c74663e7526ae2ec69662223e
ALTELMA <phongthorn.a@gmail.com>
2014-05-30 13:10:40 +0700

    Fixed nothing return error.
    Fixed nothing return error when empty date ex. "0000-00-00".

--- myDateSystem.php
+++ myDateSystem.php
@@ -1,77 +1,185 @@
 <?php
 
+/*
+ *
+ * FileName : mydatesystem.php
+ * Author : Phongthorn Kumkankaew
+ * Create Date : 18/10/2555
+ * Update Date : 21/10/2556
+ * 
+ * version : 0.0.2a
+ * Description
+ * - Add new method "normalDate" use to generate str date for international language
+ * - Fixed to use string from @str_Month for system multi language
+ *
+ */
+
 class MyDateSystem{
-	
+
 	private $day;
 	private $month;
 	private $year;
 	private $th_year;
-	
+
 	private $hour;
 	private $minute;
 	private $second;
-	
-	private $ThaiShortDate = array('มกราคม','กุมภาพันธ์','มีนาคม','เมษายน','พฤษภาคม','มิถุนายน','กรกฎาคม','สิงหาคม','กันยายน','ตุลาคม','พฤศจิกายน','ธันวาคม');
-	private $ThaiFullDate = array('ม.ค.','ก.พ.','มี.ค.','เม.ย.','พ.ค.','มิ.ย.','ก.ค.','ส.ค.','ก.ย.','ต.ค.','พ.ย.','ธ.ค.');
-	
+
+	public $str_Month = array(
+	    1 => array(
+	        'en' => 'January',
+	        'th' => 'มกราคม',
+	        'sth' => 'ม.ค.'
+	    ),
+	    2 => array(
+	        'en' => 'Febuary',
+	        'th' => 'กุมภาพันธ์',
+	        'sth' => 'ก.พ.'
+	    ),
+	    3 => array(
+	        'en' => 'March',
+	        'th' => 'มีนาคม',
+	        'sth' => 'มี.ค.'
+	    ),
+	    4 => array(
+	        'en' => 'April',
+	        'th' => 'เมษายน',
+	        'sth' => 'เม.ย.'
+	    ),
+	    5 => array(
+	        'en' => 'May',
+	        'th' => 'พฤษภาคม',
+	        'sth' => 'พ.ค.'
+	    ),
+	    6 => array(
+	        'en' => 'June',
+	        'th' => 'มิถุนายน',
+	        'sth' => 'มิ.ย.'
+	    ),
+	    7 => array(
+	        'en' => 'July',
+	        'th' => 'กรกฎาคม',
+	        'sth' => 'ก.ค.'
+	    ),
+	    8 => array(
+	        'en' => 'August',
+	        'th' => 'สิงหาคม',
+	        'sth' => 'ส.ค.'
+	    ),
+	    9 => array(
+	        'en' => 'September',
+	        'th' => 'กันยายน',
+	        'sth' => 'ก.ย.'
+	    ),
+	    10 => array(
+	        'en' => 'October',
+	        'th' => 'ตุลาคม',
+	        'sth' => 'ต.ค.'
+	    ),
+	    11 => array(
+	        'en' => 'November',
+	        'th' => 'พฤศจิกายน',
+	        'sth' => 'พ.ย.'
+	    ),
+
+	    12 => array(
+	        'en' => 'December',
+	        'th' => 'ธันวาคม',
+	        'sth' => 'ธ.ค.'
+	    ),
+	);
 
 	// Method to split date and seperate it.
 	public function splitDateTime($input){
-		
+
 		$splitDateTime = explode(' ', $input);
 		$splitDate = !empty($splitDateTime[0])?explode('-', $splitDateTime[0]):'';
 		$splitTime = !empty($splitDateTime[1])?explode(':', $splitDateTime[1]):'';
-		
+
 		// SETUP VARIABLE
 		// DATE
-		$this->day = (int)$splitDate[2];
-		$this->month = (int)$splitDate[1];
-		$this->year = $splitDate[0];
-		$this->th_year = $splitDate[0] + 543;
-		
+		$this->day = !empty($splitDate[2])?(int)$splitDate[2]:'';
+		$this->month = !empty($splitDate[1])?(int)$splitDate[1]:'';
+		$this->year = !empty($splitDate[0])?(int)$splitDate[0]:'';;
+		$this->th_year = !empty($splitDate[0])?(int)$splitDate[0] + 543:'';
+
 		// TIME
 		$this->hour = !empty($splitTime[0])?$splitTime[0]:'';
 		$this->minute = !empty($splitTime[1])?$splitTime[1]:'';
 		$this->second = !empty($splitTime[2])?$splitTime[2]:'';
 		$this->fullTime = !empty($splitDateTime[1])?$splitDateTime[1]:'';
 	}
-	
+
 	// Method to change data to default format
 	public function restoreDate($delimiter, $input){
-		
+
 		$dataSplit = explode($delimiter, $input);
 		$defaultFormat = $dataSplit[2].'/'.$dataSplit[1].'/'.$dataSplit[0];
-		
+
 		return $defaultFormat;
 	}
-			
-	// Method to change data to database
+
+	// Method to change data to thai format
 	public function thaiDate($input, $format = 1, $hasTime = FALSE){
-		
-		$this->splitDateTime($input);
-		$indexMonth = $this->month - 1;
-		
-		if($format == 1){
-			if($hasTime){
-				$newFormat = $this->day.' '.$this->ThaiShortDate[$indexMonth].' '.$this->th_year.' เวลา '.$this->fullTime.' น.';
-			}else{
-				$newFormat = $this->day.' '.$this->ThaiShortDate[$indexMonth].' '.$this->th_year;
+
+		if(!empty($input)){
+
+			$this->splitDateTime($input);
+
+			if($format == 1){
+				if($hasTime){
+					$newFormat = $this->day.' '.$this->str_Month[$this->month]['sth'].' '.$this->th_year.' เวลา '.$this->fullTime.' น.';
+				}else{
+					$newFormat = $this->day.' '.$this->str_Month[$this->month]['sth'].' '.$this->th_year;
+				}
+			}elseif($format == 2){
+				if($hasTime){
+					$newFormat = $this->day.' '.$this->str_Month[$this->month]['th'].' พ.ศ. '.$this->th_year.' เวลา '.$this->fullTime.' น.';
+				}else{
+					$newFormat = $this->day.' '.$this->str_Month[$this->month]['th'].' พ.ศ. '.$this->th_year;
+				}
+			}elseif($format == 3){
+				if($hasTime){
+					$newFormat = $this->str_Month[$this->month]['th'].' พ.ศ. '.$this->th_year.' เวลา '.$this->fullTime.' น.';
+				}else{
+					$newFormat = $this->str_Month[$this->month]['th'].' พ.ศ. '.$this->th_year;
+				}
 			}
-		}elseif($format == 2){
-			if($hasTime){
-				$newFormat = $this->day.' '.$this->ThaiFullDate[$indexMonth].' พ.ศ. '.$this->th_year.' เวลา '.$this->fullTime.' น.';
-			}else{
-				$newFormat = $this->day.' '.$this->ThaiFullDate[$indexMonth].' พ.ศ. '.$this->th_year;
-			}
-		}elseif($format == 3){
-			if($hasTime){
-				$newFormat = $this->ThaiFullDate[$indexMonth].' พ.ศ. '.$this->year.' เวลา '.$this->fullTime.' น.';
-			}else{
-				$newFormat = $this->ThaiFullDate[$indexMonth].' พ.ศ. '.$this->year;
+
+			return $newFormat;
+		}
+
+		return;
+	}
+
+	// Method to change data to normal format
+	public function normalDate($input, $format = 1, $hasTime = FALSE){
+
+		if(!empty($input)){
+
+			$this->splitDateTime($input);
+
+			if(!empty($this->day) && !empty($this->month) && !empty($this->year)){
+
+				if($format == 1){
+					if($hasTime){
+						$newFormat = $this->day.' '.$this->str_Month[$this->month]['en'].' '.$this->year.' '.$this->fullTime;
+					}else{
+						$newFormat = $this->day.' '.$this->str_Month[$this->month]['en'].' '.$this->year;
+					}
+				}elseif($format == 2){
+					if($hasTime){
+						$newFormat = $this->str_Month[$this->month]['en'].' '.$this->year.' '.$this->fullTime;
+					}else{
+						$newFormat = $this->str_Month[$this->month]['en'].' '.$this->year;
+					}
+				}
+
+				return $newFormat;
 			}
 		}
-		
-		return $newFormat;
-	}	
+
+		return;
+	}
 }
 ?>
